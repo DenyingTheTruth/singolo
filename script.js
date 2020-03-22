915864bc8984c8a26b0874fc4fd214a3226f33c3
DenyingTheTruth <anton.bachikin@gmail.com>
2020-03-22 17:20:08 +0300

    feat: complete singolo-responsive

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 "use strict";
 
 //* HEADER *
+const menu = document.getElementById("menu_toggle");
+
+if (document.documentElement.getBoundingClientRect().width <= 768) {
+  menu.addEventListener("change", () => {
+    if (menu.checked) {
+      document.querySelector("body").classList.add("prevent-scroll");
+    } else {
+      document.querySelector("body").classList.remove("prevent-scroll");
+    }
+  });
+  document.querySelector("header nav").addEventListener("click", () => {
+    document.getElementById("menu_toggle").checked = false;
+    document.querySelector("body").classList.remove("prevent-scroll");
+  });
+  document.querySelector("header nav ul").addEventListener("click", e => {
+    if (e.target.tagName === "A") {
+      document.getElementById("menu_toggle").checked = false;
+      document.querySelector("body").classList.remove("prevent-scroll");
+    } else {
+      e.stopPropagation();
+    }
+  });
+}
+
 document.addEventListener("scroll", () => {
   let curPos = window.scrollY;
   const anchors = document.querySelectorAll(".section");
@@ -32,74 +56,102 @@ document.addEventListener("scroll", () => {
 });
 
 //* SLIDER *
-let sliderItems = document.querySelector(".slides"),
-  prev = document.querySelector(".arrow-left"),
-  next = document.querySelector(".arrow-right");
+const beforeInitSliderWrapper = `<div class="slide">
+<img src="./assets/images/slider/slider-1.png" alt="Slider 1" />
+<div class="black-screen b-screen-1"></div>
+<div class="home-btn h-btn-1"></div>
+<div class="black-screen b-screen-2"></div>
+<div class="home-btn h-btn-2"></div>
+</div>
+<div class="slide">
+<img src="./assets/images/slider/slider-2.png" alt="Slider 2" />
+<div class="black-screen b-screen-3"></div>
+<div class="home-btn h-btn-3"></div>
+</div>`;
+let posInitial,
+  index = 0,
+  allowShift = true,
+  sliderItems = document.querySelector(".slides"),
+  slides = sliderItems.querySelectorAll(".slide"),
+  slidesLength = slides.length;
 
-function slide(items, prev, next) {
-  let posInitial,
-    slides = items.querySelectorAll(".slide"),
-    slidesLength = slides.length,
-    slideSize = items.getElementsByClassName("slide")[0].offsetWidth,
-    firstSlide = slides[0],
+function initSlider() {
+  sliderItems = document.querySelector(".slides");
+  slides = sliderItems.querySelectorAll(".slide");
+  slidesLength = slides.length;
+
+  let firstSlide = slides[0],
     lastSlide = slides[slidesLength - 1],
     cloneFirst = firstSlide.cloneNode(true),
-    cloneLast = lastSlide.cloneNode(true),
-    index = 0,
-    allowShift = true;
+    cloneLast = lastSlide.cloneNode(true);
 
   cloneFirst.querySelectorAll("div").forEach(el => el.remove());
   cloneLast.querySelectorAll("div").forEach(el => el.remove());
-  items.appendChild(cloneFirst);
-  items.insertBefore(cloneLast, firstSlide);
-
-  prev.addEventListener("click", function() {
-    shiftSlide(-1);
-  });
-  next.addEventListener("click", function() {
-    shiftSlide(1);
-  });
-
-  items.addEventListener("transitionend", checkIndex);
-
-  function shiftSlide(dir, action) {
-    items.classList.add("shifting");
-
-    if (allowShift) {
-      if (!action) {
-        posInitial = items.offsetLeft;
-      }
-
-      if (dir == 1) {
-        items.style.left = posInitial - slideSize + "px";
-        index++;
-      } else if (dir == -1) {
-        items.style.left = posInitial + slideSize + "px";
-        index--;
-      }
-    }
-
-    allowShift = false;
-  }
-
-  function checkIndex() {
-    items.classList.remove("shifting");
-
-    if (index == -1) {
-      items.style.left = -(slidesLength * slideSize) + "px";
-      index = slidesLength - 1;
-    }
-
-    if (index == slidesLength) {
-      items.style.left = -(1 * slideSize) + "px";
-      index = 0;
-    }
-
-    allowShift = true;
-  }
+  sliderItems.appendChild(cloneFirst);
+  sliderItems.insertBefore(cloneLast, firstSlide);
 }
 
-slide(sliderItems, prev, next);
+function checkIndex() {
+  let slideSize = sliderItems.querySelector(".slide").offsetWidth;
+  sliderItems.classList.remove("shifting");
+
+  if (index == -1) {
+    sliderItems.style.left = -(slidesLength * slideSize) + "px";
+    index = slidesLength - 1;
+  }
+
+  if (index == slidesLength) {
+    sliderItems.style.left = -(1 * slideSize) + "px";
+    index = 0;
+  }
+
+  allowShift = true;
+}
+
+function shiftSlide(dir, action) {
+  let slideSize = sliderItems.querySelector(".slide").offsetWidth;
+  sliderItems.classList.add("shifting");
+
+  if (allowShift) {
+    if (!action) {
+      posInitial = sliderItems.offsetLeft;
+    }
+
+    if (dir == 1) {
+      sliderItems.style.left = posInitial - slideSize + "px";
+      index++;
+    } else if (dir == -1) {
+      sliderItems.style.left = posInitial + slideSize + "px";
+      index--;
+    }
+  }
+
+  allowShift = false;
+}
+
+document.querySelector(".slides").addEventListener("transitionend", checkIndex);
+
+document.querySelector(".arrow-left").addEventListener("click", () => {
+  shiftSlide(-1);
+  console.log("left");
+});
+document.querySelector(".arrow-right").addEventListener("click", () => {
+  shiftSlide(1);
+  console.log("right");
+});
+
+// first slider init
+initSlider();
+
+// re-init slider to change orientation or viewport size
+window.addEventListener("resize", () => {
+  document.querySelector(
+    ".slides-wrapper .slides"
+  ).innerHTML = beforeInitSliderWrapper;
+  initSlider();
+  document.querySelector(".slides-wrapper .slides").style.left = "-100%";
+  index = 0;
+});
 
 // black screens
 let screens = document.querySelectorAll(".black-screen");
